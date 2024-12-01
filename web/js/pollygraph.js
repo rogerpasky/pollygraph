21b4f09e7f2440f673e3bb5330e06e0d645fcdc1
pascual.dejuan <pascual.dejuan@bbva.com>
2024-12-02 00:02:46 +0100

    feat: first try to get bookmark friendly navigation

--- web/js/pollygraph.js
+++ web/js/pollygraph.js
@@ -7,7 +7,18 @@ const model = new Model();
 const view = new View();
 const controller = new Controller(model, view);
 
+const rootPath = window.location.pathname;
 
-export function pollygraph(datasource) {    
-    model.setDataFromSource(datasource);
+
+export function pollygraph(initialDatasource) {    
+    model.setDataFromSource(initialDatasource);
+    window.addEventListener('popstate', onUrlChange)
+}
+
+function onUrlChange() {
+    const path = window.location.pathname;
+    if (path.startsWith(rootPath)) {
+        const datasource = path.replace(rootPath, '');
+        model.setDataFromSource(datasource);
+    }
 }
